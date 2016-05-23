57fa9b61add8a8f97274f2eba70ac609d115f0e7
kingshy <kingshy@sayhello.com>
2016-05-23 14:07:13 -0700

    prod_pill_data schema

--- configurations/pill_data.sql
+++ configurations/pill_data.sql
@@ -1,3 +1,5 @@
+
+-- dev version
 CREATE TABLE dev_pill_data (
   account_id BIGINT,
   external_tracker_id VARCHAR(100),
@@ -14,5 +16,26 @@ COMPOUND SORTKEY (local_utc_ts, account_id);
 GRANT ALL ON dev_pill_data TO migrator;
 GRANT SELECT ON dev_pill_data TO GROUP data;
 
--- run this sql later when ready for prod
--- CREATE TABLE prod_pill_data (like dev_pill_data);
+ALTER TABLE dev_pill_data OWNER TO migrator;
+
+
+-- prod version
+
+CREATE TABLE prod_pill_data (
+  account_id BIGINT,
+  external_tracker_id VARCHAR(100),
+  svm_no_gravity INTEGER,
+  ts TIMESTAMP WITHOUT TIME ZONE,
+  offset_millis INTEGER,
+  local_utc_ts TIMESTAMP WITHOUT TIME ZONE,
+  motion_range BIGINT,
+  kickoff_counts INTEGER,
+  on_duration_seconds INTEGER
+) DISTSTYLE KEY DISTKEY (account_id)
+COMPOUND SORTKEY (local_utc_ts, account_id);
+
+GRANT ALL ON prod_pill_data TO migrator;
+GRANT SELECT ON prod_pill_data TO GROUP data;
+GRANT SELECT ON prod_pill_data TO GROUP prod;
+
+ALTER TABLE prod_pill_data OWNER TO migrator;
