eccb3a4c2f1215e89bdb0dc192858606621d6884
kingshy <kingshy@sayhello.com>
2016-06-09 15:59:19 -0700

    rm old files

--- snapshots/copy_feedback.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-psql -h sensors2.cy7n0vzxfedi.us-east-1.redshift.amazonaws.com -U migrator -p 5439 -d sensors1 << EOF
-COPY timeline_feedback FROM 's3://hello-db-exports/snapshots/timeline_feedback.csv' credentials 'aws_access_key_id=AKIAIYSCDJAGNVXVWN4Q;aws_secret_access_key=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' CSV
-EOF
