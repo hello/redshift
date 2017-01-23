5e08ac410cd42ae140aec803324d5675cca3cb00
kingshy <kingshy@sayhello.com>
2017-01-23 13:02:56 -0800

    make it simple

--- configurations/analytics.sql
+++ configurations/analytics.sql
@@ -7,18 +7,14 @@
 -- * use interleaved sortkey which is better for ad-hoc queries.
 --
 
-CREATE TABLE account_actions (
+CREATE TABLE actions (
   account_id INTEGER,
-  action VARCHAR(256) NOT NULL,
-  category VARCHAR(256) DEFAULT NULL, -- ops, app, mobile, fw, ds, biz etc...
-  result VARCHAR(256) DEFAULT NULL, -- result of action if exist. e.g. pass/fail/value
-  sense_id VARCHAR(64) DEFAULT NULL,
-  fw INTEGER DEFAULT NULL, -- firmware version
-  hw INTEGER DEFAULT NULL, -- hardware version  1 or 4
-  ts TIMESTAMP WITHOUT TIME ZONE, -- utc 
+  action VARCHAR(256) NOT NULL,      -- action that user is performing
+  result VARCHAR(256) DEFAULT NULL,  -- result of action if exist. e.g. pass/fail/value
+  ts TIMESTAMP WITHOUT TIME ZONE,    -- utc 
   offset_millis INTEGER DEFAULT NULL -- local offset if available
 ) DISTYLE KEY DISTKEY (account_id)
-INTERLEAVED SORTKEY (account_id, ts, action, category, sense_id, fw_version);
+INTERLEAVED SORTKEY (account_id, ts, action);
 
 GRANT ALL ON account_actions to migrator;
 GRANT ALL ON account_actions to group ops;
