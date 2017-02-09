f968701c333f9bbcee30f176346c24375767ed5e
kingshy <kingshy@sayhello.com>
2017-02-09 13:39:26 -0800

    update sort order

--- configurations/analytics.sql
+++ configurations/analytics.sql
@@ -7,15 +7,20 @@
 -- * use interleaved sortkey which is better for ad-hoc queries.
 --
 
-CREATE TABLE actions (
+CREATE TABLE prod_actions (
   account_id BIGINT,
   action VARCHAR(256) NOT NULL,      -- action that user is performing
   result VARCHAR(256) DEFAULT NULL,  -- result of action if exist. e.g. pass/fail/value
   ts TIMESTAMP WITHOUT TIME ZONE,    -- utc 
   offset_millis INTEGER DEFAULT NULL -- local offset if available
 ) DISTSTYLE KEY DISTKEY (account_id)
-INTERLEAVED SORTKEY (account_id, ts, action);
+INTERLEAVED SORTKEY (action, ts, account_id);
 
-GRANT ALL ON actions to migrator;
-GRANT ALL ON actions to group ops;
+GRANT ALL ON prod_actions to migrator;
+GRANT ALL ON prod_actions to group ops;
+
+CREATE TABLE dev_actions (like prod_actions);
+GRANT ALL ON dev_actions to migrator;
+GRANT ALL ON dev_actions to group ops;
+
 
